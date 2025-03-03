99b323271948e9ab8d9f0bbf58e7cb25c55ff4b6
Larafale32 <adxd22@hotmail.fr>
2025-03-03 12:41:34 +0100

    Update main.py

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,20 @@
 ## Écrivez votre code ici !
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        return f"La personne s'appelle {self.name} et a {self.age} ans"
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        return f" {super().display_details()}. Il touche un salaire de {self.salary} euros par mois"
+
+employee1 = Employee("Arnaud", 24, 4000)
+
+print(employee1.display_details())
