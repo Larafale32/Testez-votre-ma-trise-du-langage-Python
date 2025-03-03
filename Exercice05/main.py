6d4915f82f21fd6ff0d2d821cf199a2903c8e420
Larafale32 <adxd22@hotmail.fr>
2025-03-03 11:58:53 +0100

    Update main.py

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,14 @@
+"""
+Cette fonciton additionne 2 valeurs (a et b) 
+et retourne le résultat de l'addition 
+"""
 def sum(a, b):
     return a + b
+
+"""
+Cette fonction soustrait 2 valeurs (a et b) 
+et retourne le résultat de la soustraction
+"""
  
 def subtraction(a, b):
     return a - b
