8a68fc5bb7de76ef44c9f533cb1b99cfa0ad30b8
Larafale32 <adxd22@hotmail.fr>
2025-03-03 11:55:46 +0100

    Update main.py

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,9 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+voyelles = ['a', 'e', 'i', 'o', 'u', 'y']
+
+
+for mot in words:
+    total_voyelles = sum(mot.count(v) for v in voyelles)
+    tuple_mots = (mot, total_voyelles)
+    print(tuple_mots)
+
