1d80a4f50437783b8852891f12892eba9b6543e1
Larafale32 <adxd22@hotmail.fr>
2025-03-03 11:51:14 +0100

    Update main.py

--- Exercice01/main.py
+++ Exercice01/main.py
@@ -1,2 +1,9 @@
 name = input("Entrez votre nom : ")
 age = int(input("Entrez votre âge : "))
+
+def program_perso():
+    name = input("Entrer votre prénom : ")
+    age = int(input("Entrer votre age : "))
+    print(f"Bonjour, je m'appelle {name} et j'ai {age} ans.")
+
+program_perso()
