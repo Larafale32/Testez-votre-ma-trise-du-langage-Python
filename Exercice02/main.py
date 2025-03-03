8efdb66fb41e5805374f9dab6b6c7d7ec43566e6
Larafale32 <adxd22@hotmail.fr>
2025-03-03 11:53:54 +0100

    Update main.py

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,27 @@ students = {
          'Histoire': 78
      }
 }
+
+def program_student():
+    while True:
+        name = input("Entrer le nom du étudiant : ")
+        if name in students:
+            print(f"\nNotes de l'étudiant {name}:")
+            for subject, score in students[name].items():
+                print(f"- {subject}: {score}")
+                continue
+
+            choice = input("Voulez vous afficher sa moyenne ?")
+            if choice.upper() == "OUI":
+                average = sum(students[name].values()) / len(students[name])
+                print(f"La moyenne de {name} est de {average}.")
+            elif choice.upper() == "NON":
+                break
+            else:
+                print("Veuillez entrer 'OUI' ou 'NON'.")
+        else:
+            print("Cet étudiant n'existe pas.")
+
+
+
+program_student()
