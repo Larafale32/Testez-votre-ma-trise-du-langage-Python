d26e081dea95e2584d9ec5b71e01e5fd9533fca5
Larafale32 <adxd22@hotmail.fr>
2025-03-03 12:06:24 +0100

    Update main.py

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,11 @@
 ## Écrivez votre code ici !
+
+def square(number):
+    try:
+        square = number*number
+        return square
+    except:
+        print("Le paramètre doit être un nombre !")
+        return None
+
+print(square("e"))
